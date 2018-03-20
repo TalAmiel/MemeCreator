734cc7277994f503c8b28f6eed5790d55a90a533
Omri Zentner <omrize@gmail.com>
2018-03-20 19:24:29 +0200

    arrange_css_01

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 console.log ('koko');
 
 
+var gCurrImg = '';
+
 var gImgs = [{
     id: 1,
     url: 'img/meme1.jpg',
@@ -33,12 +35,27 @@ function renderImgs() {
     var strHtml = '';
     console.log ('gimgs2' ,gImgs);
     gImgs.forEach(function (img) {
-        strHtml += '<div ><img src="'+img.url+'"></div>'
+        strHtml += '<div onclick="imgSelected('+img.id+')" ><img src="'+img.url+'"></div>'
         console.log ('img.url' , img['url']);
     })
     console.log ('strHtml' , strHtml);
     console.log ('elImgsContainer', elImgsContainer);
     elImgsContainer.innerHTML= strHtml;
     console.log ('elImgsContainer2', elImgsContainer);
+    
 }
+
+function imgSelected(id){
+    var elMain = document.querySelector('main');
+    elMain.classList.add('hide');
+    console.log(elMain);
+    gCurrImg = gImgs.find (function(img){
+        console.log ('img' , img.id);
+        console.log ('id' , id);
+        return img.id === id;
+    })
+    
+}
+
+
 
